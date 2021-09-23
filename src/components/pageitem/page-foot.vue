8c4316eabefa07ac178934727b9239bb32391dd0
Jiubei0408 <757795210@qq.com>
2021-09-23 23:42:03 +0800

    添加备案号

--- src/components/pageitem/page-foot.vue
+++ src/components/pageitem/page-foot.vue
@@ -3,6 +3,7 @@
         <p>
             Copyright &copy; 2018-{{ new Date().getFullYear() }} ZUCC-ACM-LAB. All Rights Reserved.<br>
             Powered by <el-link type="primary" href="https://github.com/taoting1234/acm-registration-system-backend">Taoting</el-link> & <el-link type="primary" href="https://github.com/KeadinZhou/acm-registration-system-web">Keadin</el-link>
+          ICP备案号: <a href="https://beian.miit.gov.cn/" target="_blank">浙ICP备2021027826号-1</a>
         </p>
     </div>
 </template>
